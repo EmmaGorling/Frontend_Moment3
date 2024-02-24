7e9b6834d792bb8e4f8895b844b141432618b59f
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-02-24 20:07:54 +0100

    Är något på spåren i kartorna

---
+++ src/js/map.js
@@ -0,0 +1,45 @@
+"use strict"
+
+window.onload = init;
+
+function init () {
+    showMap(); 
+}
+
+
+function showMap() {
+    let map = L.map('map').setView([58.2057984, 15.9875072], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // Element & eventlistener för sökfunktion
+    const searchEl = document.getElementById('search');
+    const searchBtn = document.getElementById('searchBtn');
+    searchBtn.addEventListener('click', () => {
+        searchCity(searchEl.value);
+    });
+}
+
+// Sök med fetchanrop
+function searchCity(input) {
+    fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${input}`)
+        .then(result=>result.json())
+        .then(place => {
+            showCity(place);
+        });
+}
+
+function showCity(place) {
+    
+    //Filtrera ut stad
+    const city = place.filter((item) => item.type === 'city');
+    
+    // Longitud och latitud för första staden som man får tillbaka
+    const lat = city[0].lat;
+    const lon = city[0].lon;
+
+    
+}
